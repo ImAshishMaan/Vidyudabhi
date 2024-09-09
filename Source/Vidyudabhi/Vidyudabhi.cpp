a24b41f5e4e6ff97a319ff61e7e067064d054c63
Ashish <ashishmaan22@gmail.com>
2024-09-09 19:49:23 +0530

    init

---
+++ Source/Vidyudabhi/Vidyudabhi.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Vidyudabhi.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Vidyudabhi, "Vidyudabhi" );
